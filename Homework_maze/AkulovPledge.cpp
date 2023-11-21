204442aa808a4e6efd51f48599eeb5bbf167e7f0
EmelyanPugachev <akula.vladik@gmail.com>
2023-11-21 23:04:32 +0300

    Update AkulovPledge.cpp

--- Homework_maze/AkulovPledge.cpp
+++ Homework_maze/AkulovPledge.cpp
@@ -5,96 +5,65 @@
 using namespace std;
 const int rows = 12;
 const int cols = 12;
-vector<vector<int>> maze_copy;
+vector<vector<int>> maze_copy_pledge;
 // Функция для печати лабиринта
-void printMaze(const std::vector<std::vector<int>>& maze) {
+void printMaze(const vector<vector<int>>& maze) {
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            std::cout << maze[i][j] << " ";
+            cout << maze[i][j] << " ";
         }
-        std::cout << std::endl;
+        cout << endl;
     }
 }
-void CopyMaze(const std::vector<std::vector<int>>& maze) {
-    maze_copy = maze;
+void CopyMaze(const vector<vector<int>>& maze) {
+    maze_copy_pledge = maze;
 }
 // Функция для проверки возможности движения в заданной точке
-bool isValidMove(const std::vector<std::vector<int>>& maze_copy, int x, int y) {
-    return x >= 0 && x < rows && y >= 0 && y < cols && maze_copy[x][y] == 0;
+bool isValidMove(const vector<std::vector<int>>& maze_copy_pledge, int x, int y) {
+    return x >= 0 && x < rows && y >= 0 && y < cols && maze_copy_pledge[x][y] == 0;
 }
 // Функция для поиска пути в лабиринте с использованием DFS
-bool findPathDFS(std::vector<std::vector<int>>& maze_copy, int x, int y) {
-    if (maze_copy[x][y] == 4) {
+bool findPathDFS(vector<vector<int>>& maze_copy_pledge, int x, int y) {
+    if (maze_copy_pledge[x][y] == 4) {
         // Достигнут выход из лабиринта
         return true;
     }
 
-    if (isValidMove(maze_copy, x, y)) {
+    if (isValidMove(maze_copy_pledge, x, y)) {
         // Помечаем текущую ячейку как посещенную
-        maze_copy[x][y] = 2;
+        maze_copy_pledge[x][y] = 2;
 
         // Пробуем двигаться во всех возможных направлениях
-        if (findPathDFS(maze_copy, x + 1, y) || findPathDFS(maze_copy, x - 1, y) ||
-            findPathDFS(maze_copy, x, y + 1) || findPathDFS(maze_copy, x, y - 1)) {
+        if (findPathDFS(maze_copy_pledge, x + 1, y) || findPathDFS(maze_copy_pledge, x - 1, y) ||
+            findPathDFS(maze_copy_pledge, x, y + 1) || findPathDFS(maze_copy_pledge, x, y - 1)) {
             // Если путь найден, помечаем ячейку как часть пути
-            maze_copy[x][y] = 3;
+            maze_copy_pledge[x][y] = 3;
             return true;
         }
 
         // Если из текущей ячейки нет пути, возвращаемся назад
-        maze_copy[x][y] = 0;
+        maze_copy_pledge[x][y] = 0;
     }
 
     return false;
 }
 
-//int main() {
-//    // Инициализация лабиринта
-//
-//    std::vector<std::vector<int>> maze = {
-//                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-//                {1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 4},
-//                {1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1},
-//                {1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1},
-//                {1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1},
-//                {1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1},
-//                {1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1},
-//                {1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1},
-//                {1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1},
-//                {1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-//                {0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1},
-//                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-//    };
-//
-//    CopyMaze(maze);
-//
-//    std::cout << "Maze before the way:" << std::endl;
-//    printMaze(maze);
-//
-//    // Начинаем поиск пути с точки входа (10, 0)
-//    if (findPathDFS(maze_copy, 10, 0)) {
-//        std::cout << "\nShow me the way:" << std::endl;
-//        printMaze(maze);
-//        cout << endl << "===============================" << endl;
-//        printMaze(maze_copy);
-//    }
-//    else {
-//        std::cout << "\nThere is no way." << std::endl;
-//    }
-//
-//    return 0;
-//}
+void maze_main_pledge(const vector<vector<int>>& maze) {
+    // Инициализация лабиринта
 
-//int* ReturnCopyMaze(std::vector<std::vector<int>>& maze) {
-//    if (findPathDFS(maze_copy, 10, 0)) {
-//        std::cout << "\nShow me the way:" << std::endl;
-//        //printMaze(maze);
-//        //cout << endl << "===============================" << endl;
-//        //printMaze(maze_copy);
-//    }
-//    //else {
-//      //  std::cout << "\nThere is no way." << std::endl;
-//    //}
-//
-//    return ;
-//}
+    CopyMaze(maze);
+    cout << "Maze before the way:" << endl;
+    printMaze(maze);
+
+    // Начинаем поиск пути с точки входа (10, 0)
+    if (findPathDFS(maze_copy_pledge, 10, 0)) {
+        cout << "\nShow me the way:" << endl;
+        printMaze(maze);
+        cout << endl << "===============================" << endl;
+        printMaze(maze_copy_pledge);
+    }
+    else {
+        cout << "\nThere is no way." << endl;
+    }
+
+}
