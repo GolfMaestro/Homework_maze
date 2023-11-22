523e9fe51a096bafdfa0a3f22b70d9283d233024
Saba2004 <116124380+Saba2004@users.noreply.github.com>
2023-11-22 18:29:57 +0300

    Add files via upload

---
+++ Homework_maze/Sabaev_rec.h
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+extern vector<vector<int>> maze_copy_rec;
+bool maze_main_rec(const vector<vector<int>>& maze);
+bool findPath(vector<vector<int>>& maze_copy_rec, int x, int y);
+void CopyMaze_Rec(const vector<vector<int>>& maze);
+void printMaze(const vector<vector<int>>& maze);
