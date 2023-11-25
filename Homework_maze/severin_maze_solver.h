18c8cac5659bdabfb2efbeae36268793f1ae6013
LawrentySeverin <140348036+LawrentySeverin@users.noreply.github.com>
2023-11-25 23:25:34 +0300

    Update severin_maze_solver.h

--- Homework_maze/severin_maze_solver.h
+++ Homework_maze/severin_maze_solver.h
@@ -1,15 +1,11 @@
-// severin_maze_solver.h
-#ifndef SEVERIN_MAZE_SOLVER_H
-#define SEVERIN_MAZE_SOLVER_H
-
+#pragma once
+#include <iostream>
 #include <vector>
 
-void initSeverinMaze(std::vector<std::vector<int>>& maze, int rows, int cols);
-void copyMaze(const std::vector<std::vector<int>>& source, std::vector<std::vector<int>>& destination);
-bool solveDeadEndFilling(std::vector<std::vector<int>>& maze);
+extern std::vector<std::vector<int>> maze_copy_deadend;
 
-bool isSafe(const std::vector<std::vector<int>>& maze, int row, int col);
-bool hasDeadEnd(const std::vector<std::vector<int>>& maze, int row, int col);
-void fillDeadEnd(std::vector<std::vector<int>>& maze, int row, int col);
-
-#endif // SEVERIN_MAZE_SOLVER_H
+void CopyMaze_deadend(const std::vector<std::vector<int>>& maze);
+bool isSafe(int row, int col, const std::vector<std::vector<int>>& maze_copy_deadend, std::vector<std::vector<bool>>& visited);
+bool isDeadEnd(int row, int col, const std::vector<std::vector<int>>& maze_copy_deadend);
+void fillDeadEnds(std::vector<std::vector<int>>& maze_copy_deadend);
+bool maze_main_deadend(const std::vector<std::vector<int>>& maze);
