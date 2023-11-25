16537fef7e9ac5f30d86bdf5d99afcd84794281d
LawrentySeverin <140348036+LawrentySeverin@users.noreply.github.com>
2023-11-25 23:26:26 +0300

    Update severin_maze_solver.cpp

--- Homework_maze/severin_maze_solver.cpp
+++ Homework_maze/severin_maze_solver.cpp
@@ -1,50 +1,100 @@
-// severin_maze_solver.cpp
-#include "severin_maze_solver.h"
+#include <iostream>
+#include <vector>
 
-void initSeverinMaze(std::vector<std::vector<int>>& maze, int rows, int cols) {
-    // Инициализация лабиринта (ваш код инициализации)
-    maze = std::vector<std::vector<int>>(rows, std::vector<int>(cols));
+#define ROW 12
+#define COL 12
+
+using namespace std;
+
+vector<vector<bool>> visited(ROW, vector<bool>(COL, false));
+vector<vector<int>> maze_copy_wallfollower;
+
+void CopyMaze_wallfollower(const vector<vector<int>>& maze) {
+    maze_copy_wallfollower = maze;
 }
 
-void copyMaze(const std::vector<std::vector<int>>& source, std::vector<std::vector<int>>& destination) {
-    destination = source;
+bool isSafe(int row, int col, const vector<vector<int>>& maze_copy_wallfollower, vector<vector<bool>>& visited) {
+    return (row >= 0) && (row < ROW) && (col >= 0) && (col < COL) && (maze_copy_wallfollower[row][col] != 1) && !visited[row][col];
 }
 
-bool isSafe(const std::vector<std::vector<int>>& maze, int row, int col) {
-    int m = maze.size();
-    int n = maze[0].size();
-    return (row >= 0 && row < m && col >= 0 && col < n && maze[row][col] == 0);
+// Новая функция для проверки, является ли клетка тупиковой
+bool isDeadEnd(int row, int col, const vector<vector<int>>& maze_copy_wallfollower) {
+    int wallCount = 0;
+    int directions[4][2] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+
+    for (int i = 0; i < 4; ++i) {
+        int nextDirX = row + directions[i][0];
+        int nextDirY = col + directions[i][1];
+
+        if (nextDirX >= 0 && nextDirX < ROW && nextDirY >= 0 && nextDirY < COL && maze_copy_wallfollower[nextDirX][nextDirY] == 1) {
+            wallCount++;
+        }
+    }
+
+    return wallCount >= 3; // Клетка считается тупиковой, если у неё 3 или более стен
 }
 
-bool hasDeadEnd(const std::vector<std::vector<int>>& maze, int row, int col) {
-    int count = 0;
+bool findExit(vector<vector<int>>& maze_copy_wallfollower, int row, int col, int exitRow, int exitCol, vector<vector<bool>>& visited) {
+    if (row == exitRow && col == exitCol) {
+        return true;
+    }
 
-    if (!isSafe(maze, row + 1, col)) count++;
-    if (!isSafe(maze, row - 1, col)) count++;
-    if (!isSafe(maze, row, col + 1)) count++;
-    if (!isSafe(maze, row, col - 1)) count++;
+    visited[row][col] = true;
 
-    return (count >= 3);
-}
+    int directions[4][2] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
 
-void fillDeadEnd(std::vector<std::vector<int>>& maze, int row, int col) {
-    maze[row][col] = 1;
-}
+    for (int i = 0; i < 4; ++i) {
+        int nextDirX = row + directions[i][0];
+        int nextDirY = col + directions[i][1];
 
-bool solveDeadEndFilling(std::vector<std::vector<int>>& maze) {
-    int m = maze.size();
-    int n = maze[0].size();
+        if (isSafe(nextDirX, nextDirY, maze_copy_wallfollower, visited)) {
+            maze_copy_wallfollower[nextDirX][nextDirY] = 2;
 
-    std::vector<std::vector<int>> severin_maze_copy(m, std::vector<int>(n, 0));
-    copyMaze(maze, severin_maze_copy);
-
-    for (int i = 1; i < m - 1; ++i) {
-        for (int j = 1; j < n - 1; ++j) {
-            if (hasDeadEnd(severin_maze_copy, i, j)) {
-                fillDeadEnd(maze, i, j);
+            if (findExit(maze_copy_wallfollower, nextDirX, nextDirY, exitRow, exitCol, visited)) {
+                return true;
             }
         }
     }
 
+    if (isDeadEnd(row, col, maze_copy_wallfollower)) {
+        maze_copy_wallfollower[row][col] = 1; // Помечаем тупиковые клетки стеной
+    }
+
     return false;
+}
+
+bool maze_main_wallfollower(const vector<vector<int>>& maze) {
+    CopyMaze_wallfollower(maze);
+    int startX = 0, startY = 0;
+    int exitX = 0, exitY = 0;
+
+    for (int i = 0; i < ROW; ++i) {
+        for (int j = 0; j < COL; ++j) {
+            if (maze[i][j] == 0) {
+                startX = i;
+                startY = j;
+            }
+            else if (maze[i][j] == 4) {
+                exitX = i;
+                exitY = j;
+            }
+        }
+    }
+
+    if (findExit(maze_copy_wallfollower, startX, startY, exitX, exitY, visited)) {
+        cout << "Успех!" << endl;
+    }
+    else {
+        cout << "Не удалось!" << endl;
+    }
+
+    // Вывод лабиринта с отмеченным путем
+    for (int i = 0; i < ROW; ++i) {
+        for (int j = 0; j < COL; ++j) {
+            cout << maze_copy_wallfollower[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
