8490aead49a320da1baaf124de7750c75a9361ca
Vlad Eryn <vlad2004e@gmail.com>
2023-11-26 23:45:15 +0300

    function for print solution for maze

--- Homework_maze/maze_solve.cpp
+++ Homework_maze/maze_solve.cpp
@@ -1,3 +1,78 @@
+#include <iostream>
+#include <vector>
+#include "AkulovPledge.h"
+#include "NaschokinWave.h"
+#include "SamokrutovWallFollower.h"
+#include "Sabaev_rec.h"
+#include "Tremaux.h"
+#include "severin_maze_solver.h"
+
+using namespace std;
+
+vector<vector<int>> maze = { {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 4},
+                {1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1},
+                {1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1},
+                {1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1},
+                {1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1},
+                {1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1},
+                {1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1},
+                {1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1},
+                {1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+                {0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1} };
+
+
+void printMaze(const vector<vector<int>>& maze, int rows, int columns) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            cout << maze[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+
 int main() { // the only one main in project
-	return 0;
+
+    cout << "Source maze: " << endl;
+    printMaze(maze, 12, 12);
+    cout << "============================" << endl;
+    cout << endl;
+
+    cout << "Akulov Vladislav. Pledge algorithm solution: " << endl;
+    maze_main_pledge(maze);
+    printMaze(maze_copy_pledge, 12, 12);
+    cout << "============================" << endl;
+    cout << endl;
+
+    cout << "Andrey Naschokin. Wave algorithm solution: " << endl;
+    maze_main_wave(maze);
+    printMaze(maze_copy_wave, 12, 12);
+    cout << "============================" << endl;
+    cout << endl;
+
+    cout << "Nikolay Samokrutov. Wallfollower algorithm solution: " << endl;
+    maze_main_wallfollower(maze);
+    printMaze(maze_copy_wallfollower, 12, 12);
+    cout << "============================" << endl;
+    cout << endl;
+
+    cout << "Sabaev Nikita. Recursive algorithm solution: " << endl;
+    maze_main_rec(maze);
+    printMaze(maze_copy_rec, 12, 12);
+    cout << "============================" << endl;
+    cout << endl;
+
+    cout << "Korotkov Artem. Tremaux algorithm solution: " << endl;
+    printMaze(Tremaux(maze), 12, 12);
+    cout << "============================" << endl;
+    cout << endl;
+
+    cout << "Severin Lavrenty. Dead-end algorithm solution: " << endl;
+    maze_main_deadend(maze);
+    printMaze(maze_copy_deadend, 12, 12);
+    cout << "============================" << endl;
+    cout << endl;
+
 }
